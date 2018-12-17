6a1cae02471267aab1282ac352dffdf907b0ad96
BSalim <burhan.salman@gmail.com>
2018-12-17 02:26:48 +0000

    add type, find method and differentiate method

--- node.h
+++ node.h
@@ -3,21 +3,31 @@
 
 #include <string>
 #include <iostream>
+enum Type {OPERATOR, NUMBER, UNKNOWN};
+
 class Node {
   private:
     std::string value;
+    Type type;
     Node* right;
     Node* left;
+    bool isNumber(std::string str) {
+      for (int i = 0; i < str.length(); i++) {
+        if (!std::isdigit(str[i])) return false;
+      }
+      return true;
+    }
+    bool isOperator(std::string str) {
+      return str == "+" || str == "-" || str == "*" || str == "/" || str == "^";
+    }
   public:
     Node(std::string value) {
       this->value = value;
       this->right = NULL;
       this->left = NULL;
-    }
-    Node(std::string value, Node* left, Node* right) {
-      this->value = value;
-      this->right = right;
-      this->left = left;
+      if (isOperator(value)) this->type = OPERATOR;
+      else if (isNumber(value)) this->type = NUMBER;
+      else this->type = UNKNOWN;
     }
     Node* setRight(Node* right) {
       this->right = right;
@@ -36,6 +46,61 @@ class Node {
     Node* getRight() {
       return this->right;
     }
+    Type getType() {
+      return this->type;
+    }
+    bool find(std::string value) {
+      if (this->value == value) return true;
+      else if (!this->right) return false;
+      else {
+        return this->left->find(value) || this->right->find(value);
+      }
+    }
+    void print(std::string spaces) {
+      std::cout << spaces << this->value ;
+      if (this->type == NUMBER) std::cout << " NUMBER" << std::endl;
+      else if (this->type == OPERATOR) std::cout << " OPERATOR" << std::endl;
+      else if (this->type == UNKNOWN) std::cout << " UNKNOWN" << std::endl;
+      else std::cout << " ERROR" << std::endl;
+      spaces = spaces + "| ";
+      if (this->right && this->left){
+        this->left->print(spaces);
+        this->right->print(spaces);
+      }
+    }
+
+    std::string getString() {
+      //TODO : get the expression with brackets of current node
+      return "str";
+    }
+
+    std::string differentiate(std::string wrt) {
+    if (this->type == NUMBER) {
+      return "0";
+    } else if (this->type == UNKNOWN) {
+      if (this->value == wrt) return "1";
+      else return "0";
+    } else if (this->type == OPERATOR) {
+      if (!this->find(wrt)) return "0";
+      std::string op = this->value;
+      if (op == "+" || op == "-") {
+        std::string left = this->left->differentiate(wrt);
+        std::string right = this->right->differentiate(wrt);
+        return left + op + right;
+      } else if (op == "*") {
+        
+          return "(" + this->left->getString() + ")" + op + "(" + this->right->differentiate(wrt) + ")" + "+"  + "(" + this->left->differentiate(wrt) + ")" + op+ "(" + this->right->getString() + ")";
+
+      } else if (op == "/") {
+        //TODO : differentiation for quotients
+        return "0";
+      } else if (op == "^") {
+        //TODO : differentiation for exponents
+        return "0";
+      }
+    }
+    return "Error";
+  }
 };
 
 #endif // NODE
